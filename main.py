11c9f276b25ae8fb487308bca4166e5b095fcd42
JonnhyMarlison <renat.enokaev@mail.ru>
2019-09-25 16:22:01 +0300

    Fixed barcode detect.

--- main.py
+++ main.py
@@ -11,26 +11,33 @@ import socket
 import threading
 
 TCP_PORT_SERIAL = 9000
+frame = 0
+#class Class_numpy:
+#	pts = np.object
+#	def __init__ (self):
+#		self.
 
 def barcodeReader(image, bgr):
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     barcodes = decode(gray_img)
+    pts = np.array([])
+    if len(barcodes) == 0:
+        return "NAN", pts
 
     for decodedObject in barcodes:
         points = decodedObject.polygon
-        global pts
 
         pts = np.array(points, np.int32)
         pts = pts.reshape((-1, 1, 2))
-        print(np.take(pts,([0, 0])))
-        print(np.take(pts,([4, 4])))
+        print(np.take(pts,([0])))
+        print(np.take(pts,([4])))
         cv2.polylines(image, [pts], True, (0, 0, 255), 3)
-
+    
     for bc in barcodes:
         cv2.putText(frame, bc.data.decode("utf-8") + " - " + bc.type, (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1,
                     bgr, 2)
 
-        return "Barcode: {} - Type: {}".format(bc.data.decode("utf-8"), bc.type)
+        return ("Barcode: {} - Type: {}".format(bc.data.decode("utf-8"), bc.type)), (pts)
 
 def get_name_usb():
 	dev = Popen("ls /dev/ttyU* 2>/dev/null", shell=True, stdin=PIPE, stdout=PIPE).stdout.read().split()
@@ -78,76 +85,69 @@ def video_thread():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.connect(('127.0.0.1', TCP_PORT_SERIAL))
 
-	pts = 0
 	value = -1
 	bgr = (0, 255, 0)
 	frame_rate = 30
 	camera = PiCamera()
 	camera.resolution = (640, 480)
 	rawCapture = PiRGBArray(camera, size = (640, 480))
-	time.sleep(0.1)
 
-	L_x1 = 300
-	L_y1 = 0
-	L_y2 = 480
+	L_x1 = np.int32(300)
+	L_y1 = np.int32(0)
+	L_y2 = np.int32(480)
 
-	R_x1 = 385
-	R_y1 = 0
-	R_y2 = 480
-
-	x1 = int(np.take(pts,([0])))
-	x2 = int(np.take(pts,([0])))
+	R_x1 = np.int32(385)
+	R_y1 = np.int32(0)
+	R_y2 = np.int32(480)
 
 	for frame1 in camera.capture_continuous(rawCapture, format = "bgr", use_video_port = True):   
+		global frame
 		frame = frame1.array
 		image = frame1.array
 
 		line1 = cv2.line(image, (L_x1, L_y1), (L_x1, L_y2), (0, 255, 0), 3)
 		line2 = cv2.line(image, (R_x1, L_y1), (R_x1, R_y2), (0, 255, 0), 3)
 
-		barcode = barcodeReader(frame, bgr)
+		barcode, pts = barcodeReader(image, bgr)
 		print(barcode)
+		if len(pts):
+			x1 = np.take(pts,([0]))
+			x2 = np.take(pts,([4]))
 
 		if value == -1:
 			send_to_serial(sock, 'R 0')
-			print("VALUE")
+			print("Step_0")
 
 		if barcode == "Barcode: Step_1 - Type: QRCODE":
 			send_to_serial(sock, 'L 0 0')
-			#time.sleep(1)
 			send_to_serial(sock, 'L 0 255')
-			time.sleep(1)
-
 			print("Step_1_0")
-			#value = 0
+			value = 0
 
 
-		if (x1 < L_x1 and x2 < R_x1 and barcode == "Barcode: Step_1 - Type: QRCODE"):
-			print("Step_1_1")
+		#if (x1 < L_x1 and x2 < R_x1 and barcode == "Barcode: Step_1 - Type: QRCODE"):
+			#print("Step_1_1")
 
-			send_to_serial(sock, 'L 0 0')
-			time.sleep(1)
+			#send_to_serial(sock, 'L 0 0')
 			#value = 1
-			send_to_serial(sock, 'L 0 255')
-			time.sleep(1)
+			#send_to_serial(sock, 'L 0 255')
 
-		if barcode == "Barcode: Step_2 - Type: QRCODE":
-	   		print("Step_2_0")
+		#if barcode == "Barcode: Step_2 - Type: QRCODE":
+	   		#print("Step_2_0")
 
-	   		send_to_serial(sock, 'R 1')
-	   		time.sleep(1)
+	   		#send_to_serial(sock, 'R 1')
+	   		#time.sleep(1)
 
 		if barcode == "Barcode: Step_3 - Type: QRCODE":
 			print("PING PING PING")
 
-		#cv2.imshow('Barcode reader', frame)
+		#cv2.imshow('Barcode reader', image)
 		rawCapture.truncate(0)
 
 
 		code = cv2.waitKey(10)
 		if code == ord('q'):
 			break
-		time.sleep(1)
 
 
 thread1 = threading.Thread(target=serial_thread, args=())
