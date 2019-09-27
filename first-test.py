a5ed85f18e8b27663f7e54cd62cc66e75e7ba202
JonnhyMarlison <renat.enokaev@mail.ru>
2019-09-27 07:50:01 +0300

    Add new func barcode_searcher.

--- first-test.py
+++ first-test.py
@@ -1,42 +1,44 @@
 import cv2
 import numpy as np
-import video
+from picamera import PiCamera
+from picamera.array import PiRGBArray
 
 if __name__ == '__main__':
     def callback(*arg):
         print (arg)
 
-cv2.namedWindow( "result" )
+camera = PiCamera()
+camera.resolution = (640, 480)
+rawCapture = PiRGBArray(camera, size = (640, 480))
+bgr = (0, 255, 0)
 
-cap = video.create_capture(0)
-hsv_min = np.array((53, 55, 147), np.uint8)
-hsv_max = np.array((83, 160, 255), np.uint8)
+hsv_min = np.array((0, 0, 0), np.uint8)
+hsv_max = np.array((255, 100, 30), np.uint8)
 
 color_yellow = (0,255,255)
 
-while True:
-    flag, img = cap.read()
-    img = cv2.flip(img,1) # отражение кадра вдоль оси Y
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV )
-    thresh = cv2.inRange(hsv, hsv_min, hsv_max)
+for frame1 in camera.capture_continuous(rawCapture, format = "bgr", use_video_port = True):
+	img = frame1.array
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	thresh = cv2.inRange(hsv, hsv_min, hsv_max)
 
-    moments = cv2.moments(thresh, 1)
-    dM01 = moments['m01']
-    dM10 = moments['m10']
-    dArea = moments['m00']
+	moments = cv2.moments(thresh, 1)
+	dM01 = moments['m01']
+	dM10 = moments['m10']
+	dArea = moments['m00']
 
-    if dArea > 100:
-        x = int(dM10 / dArea)
-        y = int(dM01 / dArea)
-        cv2.circle(img, (x, y), 5, color_yellow, 2)
-        cv2.putText(img, "%d-%d" % (x,y), (x+10,y-10), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, color_yellow, 2)
+	if dArea > 100:
+		x = int(dM10 / dArea)
+		y = int(dM01 / dArea)
+		cv2.circle(img, (x, y), 5, color_yellow, 2)
+		cv2.putText(img, "%d-%d" % (x,y), (x+10,y-10), 
+		cv2.FONT_HERSHEY_SIMPLEX, 1, color_yellow, 2)
 
-    cv2.imshow('result', img)
+	cv2.imshow('result', img)
+	rawCapture.truncate(0)
  
-    ch = cv2.waitKey(5)
-    if ch == 27:
-        break
+	ch = cv2.waitKey(5)
+	if ch == 27:
+		break
 
-cap.release()
 cv2.destroyAllWindows()
